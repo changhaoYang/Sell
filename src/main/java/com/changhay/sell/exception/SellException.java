224a1d2ef1a7f5d12881f8ec5fbf8145194f6cbf
Changhao Yang <changhay@andrew.cmu.edu>
2020-05-02 19:08:37 -0400

    Handle exception

--- src/main/java/com/changhay/sell/exception/SellException.java
+++ src/main/java/com/changhay/sell/exception/SellException.java
@@ -1,7 +1,9 @@
 package com.changhay.sell.exception;
 
 import com.changhay.sell.enums.ResultEnum;
+import lombok.Getter;
 
+@Getter
 public class SellException extends RuntimeException {
 
     private Integer code;
--- src/main/java/com/changhay/sell/handler/SellExceptionHandler.java
+++ src/main/java/com/changhay/sell/handler/SellExceptionHandler.java
@@ -1,10 +1,17 @@
 package com.changhay.sell.handler;
 
+import com.changhay.sell.VO.ResultVO;
 import com.changhay.sell.config.ProjectUrlConfig;
+import com.changhay.sell.exception.SellException;
 import com.changhay.sell.exception.SellerAuthorizeException;
+import com.changhay.sell.utils.ResultVOUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
@@ -22,6 +29,12 @@ public class SellExceptionHandler {
         .concat("?returnUrl=")
         .concat(projectUrlConfig.getSell())
         .concat("/sell/seller/login"));
+    }
+
+    @ExceptionHandler(value = SellException.class)
+    @ResponseBody
+    public ResultVO handleSellException(SellException e) {
+        return ResultVOUtil.error(e.getCode(), e.getMessage());
     }
 
 }
