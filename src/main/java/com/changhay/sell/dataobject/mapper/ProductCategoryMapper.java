01a812782d8fec961860fb578fe0a2c5981b1aac
Changhao Yang <changhay@andrew.cmu.edu>
2020-05-02 19:43:29 -0400

    Mybatis @

---
+++ src/main/java/com/changhay/sell/dataobject/mapper/ProductCategoryMapper.java
@@ -0,0 +1,49 @@
+package com.changhay.sell.dataobject.mapper;
+
+import com.changhay.sell.dataobject.ProductCategory;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+import java.util.Map;
+
+public interface ProductCategoryMapper {
+
+    @Insert("INSERT INTO product_category(category_name, category_type) VALUES (#{category_name, jdbcType=VARCHAR}, #{category_type, jdbcType=INTEGER})")
+    int insertByMap(Map<String, Object> map);
+
+    @Insert("INSERT INTO product_category(category_name, category_type) VALUES (#{categoryName, jdbcType=VARCHAR}, #{categoryType, jdbcType=INTEGER})")
+    int insertByObject(ProductCategory productCategory);
+
+    @Select("SELECT * FROM product_category WHERE category_type = #{categoryType}")
+    @Results({
+            @Result(column = "category_id", property = "categoryId"),
+            @Result(column = "category_name", property = "categoryName"),
+            @Result(column = "category_type", property = "categoryType")
+    })
+    ProductCategory findByCategoryType(Integer categoryType);
+
+    @Select("SELECT * FROM product_category WHERE category_name = #{categoryName}")
+    @Results({
+            @Result(column = "category_id", property = "categoryId"),
+            @Result(column = "category_name", property = "categoryName"),
+            @Result(column = "category_type", property = "categoryType")
+    })
+    List<ProductCategory> findByCategoryName(String categoryName);
+
+    @Update("UPDATE product_category SET category_name = #{categoryName} WHERE category_type = #{categoryType}")
+    int updateByCategoryType(@Param("categoryName") String categoryName,
+                             @Param("categoryType") Integer categoryType);
+
+    @Update("UPDATE product_category SET category_name = #{categoryName} WHERE category_type = #{categoryType}")
+    int updateByObject(ProductCategory productCategory);
+
+    @Delete("DELETE FROM product_category WHERE category_type = #{categoryType}")
+    int deleteByCategoryType(Integer categoryType);
+
+}
---
+++ src/test/java/com/changhay/sell/dataobject/mapper/ProductCategoryMapperTest.java
@@ -0,0 +1,76 @@
+package com.changhay.sell.dataobject.mapper;
+
+import com.changhay.sell.dataobject.ProductCategory;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Slf4j
+public class ProductCategoryMapperTest {
+
+    @Autowired
+    private ProductCategoryMapper mapper;
+
+    @Test
+    public void insertByMap() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("category_name", "师兄最不爱");
+        map.put("category_type", 101);
+        int result = mapper.insertByMap(map);
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void insertByObject() {
+        ProductCategory productCategory = new ProductCategory();
+        productCategory.setCategoryName("师兄最不爱");
+        productCategory.setCategoryType(102);
+        int result = mapper.insertByObject(productCategory);
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void findByCategoryType() {
+        ProductCategory result = mapper.findByCategoryType(102);
+        assertNotNull(result);
+    }
+
+    @Test
+    public void findByCategoryName() {
+        List<ProductCategory> result = mapper.findByCategoryName("师兄最不爱");
+        assertNotEquals(0, result.size());
+    }
+
+    @Test
+    public void updateByCategoryType() {
+        int result = mapper.updateByCategoryType("师兄最不爱的分类", 102);
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void updateByObject() {
+        ProductCategory productCategory = new ProductCategory();
+        productCategory.setCategoryName("师兄最不爱");
+        productCategory.setCategoryType(102);
+        int result = mapper.updateByObject(productCategory);
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void deleteByCategoryType() {
+        int result = mapper.deleteByCategoryType(102);
+        assertEquals(1, result);
+    }
+
+}
